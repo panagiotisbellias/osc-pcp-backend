6eb26f5fda2954b4dbfe5380f334e22ba3e6c2d9
Panagiotis Bellias <belliaspan@gmail.com>
2024-02-24 22:44:18 +0200

    Cover all public methods with unit tests

--- src/test/java/gr/pcp/GreetingResourceTest.java
+++ src/test/java/gr/pcp/GreetingResourceTest.java
@@ -1,15 +1,26 @@
 package gr.pcp;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@Disabled("database is needed")
-@QuarkusTest
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class GreetingResourceTest {
+
+    @InjectMocks
+    GreetingResource greetingResource;
+
+    @Disabled("database is needed")
     @Test
     void testHelloEndpoint() {
         given()
@@ -17,6 +28,11 @@ class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello, dear quarkus developer!"));
+    }
+
+    @Test
+    void testHello() {
+        Assertions.assertNull(greetingResource.hello());
     }
 
 }
--- src/test/java/gr/pcp/manager/PersonManagerTest.java
+++ src/test/java/gr/pcp/manager/PersonManagerTest.java
@@ -1,11 +1,14 @@
 package gr.pcp.manager;
 
 import gr.pcp.model.PersonModel;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -13,19 +16,33 @@ import org.mockito.quality.Strictness;
 
 import java.util.List;
 
-@Disabled("Ignored due to new untested functionality. Tests must be refactored")
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class PersonManagerTest {
 
+    @Mock
+    EntityManager entityManager;
+
+    @Mock
+    TypedQuery<PersonModel> typedQuery;
+
     @InjectMocks
     PersonManager personManager;
 
+    PersonModel person1;
+    PersonModel person2;
+
+    @BeforeEach
+    public void setup() {
+        person1 = Mockito.mock(PersonModel.class);
+        person2 = Mockito.mock(PersonModel.class);
+        Mockito.when(entityManager.find(PersonModel.class, 1)).thenReturn(person1);
+    }
+
     @Test
     void testGetAllPeople() {
-        PersonModel person1 = Mockito.mock(PersonModel.class);
-        PersonModel person2 = Mockito.mock(PersonModel.class);
-        Mockito.when(PersonModel.listAll()).thenReturn(List.of(person1, person2));
+        Mockito.when(entityManager.createQuery("SELECT p FROM PersonModel p", PersonModel.class)).thenReturn(typedQuery);
+        Mockito.when(typedQuery.getResultList()).thenReturn(List.of(person1, person2));
 
         List<PersonModel> people = personManager.getAllPeople();
         Assertions.assertEquals(List.of(person1, person2), people);
@@ -33,26 +50,26 @@ class PersonManagerTest {
 
     @Test
     void testGetPersonById() {
-
+        PersonModel person2 = personManager.getPersonById(1);
+        Assertions.assertEquals(person1, person2);
     }
 
     @Test
     void testCreatePerson() {
-        PersonModel person = Mockito.mock(PersonModel.class);
-        personManager.createPerson(person);
-        Mockito.verify(person).persist();
+        personManager.createPerson(person1);
+        Mockito.verify(entityManager).persist(person1);
     }
 
     @Test
     void testUpdatePerson() {
-        PersonModel person = Mockito.mock(PersonModel.class);
-        PersonModel existingPerson = personManager.updatePerson(1, person);
-        Assertions.assertEquals(person, existingPerson);
+        PersonModel person2 = personManager.updatePerson(1, person1);
+        Assertions.assertEquals(person1, person2);
     }
 
     @Test
     void testDeletePerson() {
-
+        personManager.deletePerson(1);
+        Mockito.verify(entityManager).remove(person1);
     }
 
 }
